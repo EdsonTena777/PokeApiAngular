ec11105ae97f09df1d8bb1652a383eb7c3183534
EdsonTena777 <edsontena@outlook.com>
2026-04-27 16:32:40 -0600

    Carta vuelta y paginacion

--- PokeApiEquipo/src/app/Components/get-all-poke/get-all-poke.ts
+++ PokeApiEquipo/src/app/Components/get-all-poke/get-all-poke.ts
@@ -2,6 +2,7 @@ import { Component, inject, model } from '@angular/core';
 import { PokemonService } from '../../Services/pokemon-service';
 import { Pokemon } from '../../Interfaces/pokemon-model';
 import { TitleCasePipe } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core'
 
 @Component({
   selector: 'app-get-all-poke',
@@ -11,10 +12,17 @@ import { TitleCasePipe } from '@angular/common';
 })
 export class GetAllPoke {
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
   voltea: boolean = false;
 
   presionaCarta(){
     this.voltea = !this.voltea;   
+  }
+
+  toggleFlip(pokemon: any, event: Event) {
+    event.stopPropagation();
+    pokemon.flipped = !pokemon.flipped;
   }
 
   public pokemones: Pokemon[] = [];
@@ -23,25 +31,49 @@ export class GetAllPoke {
 
   ngOnInit(): void {
     console.log('Component initialized');
-    this.GetAllPoke();
+    this.cargarPokemones();
   };
 
-  GetAllPoke() {
-    this.pokemonService.GetAllPoke().subscribe({
+  limit: number = 20;
+  offset: number = 0;
+  paginaActual: number = 1;
+  totalPaginas: number = 0;
+
+  cargarPokemones() {
+    this.pokemonService.GetAllPoke(this.limit, this.offset).subscribe({
       next: (data: any) => {
-        console.log('Datos recibidos:', data); // Mira esto en la consola F12
         if (data && data.results) {
+          console.log('Datos recibidos:', data); 
           this.pokemones = data.results.map((p: any) => ({
             name: p.name,
             url: p.url,
             idPokemon: parseInt(p.url.split('/').filter(Boolean).pop() || '0'),
             flipped: false
           }));
-          console.log('Pokemones procesados:', this.pokemones);
+
+          this.totalPaginas = Math.ceil(data.count / this.limit);
+          this.paginaActual = Math.floor(this.offset / this.limit) + 1;
+
+          this.cdr.detectChanges();
         }
       },
       error: (err) => console.error('Error al cargar:', err)
     });
   }
+
+  siguientePagina() {
+    if (this.paginaActual < this.totalPaginas) {
+      this.offset += this.limit;
+      this.cargarPokemones();
+    }
+  }
+
+  anteriorPagina() {
+    if (this.offset >= this.limit) {
+      this.offset -= this.limit;
+      this.cargarPokemones();
+    }
+  }
+
 
 }

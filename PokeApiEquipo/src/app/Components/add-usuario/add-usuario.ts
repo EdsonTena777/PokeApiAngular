f00579e3f1dabac0c13ab6747a6bd985576e7d0d
EdsonTena777 <edsontena@outlook.com>
2026-04-30 11:41:11 -0600

    vistas del login y add

--- PokeApiEquipo/src/app/Components/add-usuario/add-usuario.ts
+++ PokeApiEquipo/src/app/Components/add-usuario/add-usuario.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../../Interfaces/usuario-model';
 
 @Component({
   selector: 'app-add-usuario',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-usuario.html',
   styleUrl: './add-usuario.css',
 })
-export class AddUsuario {}
+export class AddUsuario {
+  usuario: Usuario = {
+    IdUsuario: 0,
+    UserName: '',
+    Email: '',
+    Password: '',
+    Roles: { IdRol: 0, NombreRol: '' } 
+  };
+
+  agregarUsuario() {
+    if (this.usuario.UserName && this.usuario.Email && this.usuario.Password) {
+      alert(`¡Entrenador ${this.usuario.UserName} agregado a la Pokédex!`);
+      // back para agregar usaurio
+    } else {
+      alert('Por favor completa todos los campos.');
+    }
+  }
+}
--- PokeApiEquipo/src/app/Components/login-poke/login-poke.ts
+++ PokeApiEquipo/src/app/Components/login-poke/login-poke.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-poke',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-poke.html',
   styleUrl: './login-poke.css',
 })
-export class LoginPoke {}
+export class PokemonLoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username && this.password) {
+      this.router.navigate(['/pokedex']); 
+    } else {
+      alert('Por favor ingresa tu nombre y contraseña.');
+    }
+  }
+  add(){
+    this.router.navigate(['/addUsuario'])
+  }
+}
